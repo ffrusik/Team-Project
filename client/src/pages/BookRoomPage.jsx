bb41fe8220d5d3507b0c17fb1545da92ea1964c7
Ruslan <superruslan2005@gmail.com>
2026-02-15 20:23:45 +0000

    Added a form

--- client/src/pages/BookRoomPage.jsx
+++ client/src/pages/BookRoomPage.jsx
@@ -5,8 +5,22 @@ function BookRoomPage() {
 
   return (
     <div>
-      <h1>Book Room {id}</h1>
-      {/* booking form here */}
+      <h1>Book Room #{id}</h1>
+      <form action="/api/bookings" method="POST">
+        <p>New here?</p>
+        <label for="guestNameInput">Full name: </label>
+        <input type="text" id="guestNameInput" ></input>
+        <label for="emailInput">Email: </label>
+        <input type="text" id="emailInput" ></input>
+        <label for="phoneNumberInput">Phone number: </label>
+        <input type="text" id="phoneNumberInput" ></input>
+        <label for="townInput">Town: </label>
+        <input type="text" id="townInput" ></input>
+        <label for="countyInput">County: </label>
+        <input type="text" id="countyInput" ></input>
+        <label for="eircodeInput">EirCode: </label>
+        <input type="text" id="eircodeInput" ></input>
+      </form>
     </div>
   );
 }
